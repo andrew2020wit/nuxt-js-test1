30e1b4ec7941824b69e25a7206a5b15d220e2f6d
andrew2020wit <andrew2020wit@gmail.com>
2020-12-23 13:19:15 +0200

    users list

---
+++ pages/crud-user/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div.container
+    h1 Users list
+    // pre {{users}}
+    ol
+      li(v-for="item in users" :key="item.id") {{item.name}}
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const usersExt = await $axios.$get(
+      `http://localhost:3001/api/main/crud-rest/1000/user`
+    )
+    const users = usersExt.payload
+    return { users }
+  },
+}
+</script>
