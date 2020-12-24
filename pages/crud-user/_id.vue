80a3541c0b4e6d1b7a790487fb9302f3d3e4af69
andrew2020wit <andrew2020wit@gmail.com>
2020-12-24 20:45:20 +0200

    Create User

---
+++ pages/crud-user/_id.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div.container
+    h1 User details
+    p <strong> Name: </strong> {{user.name}}
+    p <strong>Age:</strong> {{user.age}}
+</template>
+
+<script>
+import { apiUrl } from '@/const'
+export default {
+  async asyncData({ $axios, params, env }) {
+    const point = apiUrl + '/main/crud-rest/0/user/' + params.id
+    console.log('point', point)
+    const usersExt = await $axios.$get(point)
+    const user = usersExt.payload
+    return { user }
+  },
+}
+</script>
--- pages/crud-user/index.vue
+++ pages/crud-user/index.vue
@@ -1,19 +1,26 @@
 <template lang="pug">
   div.container
     h1 Users list
-    // pre {{users}}
+    CreateUser
+    h2 List
     ol
-      li(v-for="item in users" :key="item.id") {{item.name}}
+      li(v-for="item in users" :key="item.id")
+        NuxtLink(:to="baseUrl+item.id") {{item.name}}
 </template>
 
 <script>
+import { apiUrl } from '@/const'
 export default {
-  async asyncData({ $axios }) {
-    const usersExt = await $axios.$get(
-      `http://localhost:3001/api/main/crud-rest/1000/user`
-    )
+  async asyncData({ $axios, env }) {
+    const point = apiUrl + '/main/crud-rest/0/user'
+    const usersExt = await $axios.$get(point)
     const users = usersExt.payload
     return { users }
+  },
+  data() {
+    return {
+      baseUrl: '/crud-user/',
+    }
   },
 }
 </script>
